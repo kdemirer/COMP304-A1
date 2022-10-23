dee61f66cb3f559dba75114cddbb9e3851bd740b
Kaan Demirer <kaandemirer@Kaans-MacBook-Pro.local>
2022-10-23 22:20:01 +0300

    sol problem1 part1

--- problem1/part1/part1.c
+++ problem1/part1/part1.c
@@ -1,8 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <signal.h>
 
-int main()
-{
-printf("\nA sample C program\n\n");
-return 0;
+int main(){
+
+int pid = fork();
+if(pid == 0){
+// child process
+struct timeval tim;
+printf("Child process\n");
+for(;;){
+gettimeofday(&tim, NULL);
+printf("In child process: pid = %d, time of the day = %ld\n", getpid(), tim.tv_sec);
+sleep(1);
 }
 
+} else if(pid > 0) {
+// parent process
+sleep(5);
+kill(pid, SIGTERM);
+printf("Child %d killed\n", pid);
+} else {
+// fork failed
+printf("Creating child process failed\n");
+}
+
+return 0;
+}
